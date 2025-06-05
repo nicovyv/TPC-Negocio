b911faf82a93d644c18e6125efcbbe5c20a033aa
nicolas zabala <nicozb.v@gmail.com>
2025-06-04 21:38:22 -0300

    se agregan clases CategoriaNegocio y MarcaNegocio

---
+++ negocio/CategoriaNegocio.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dominio;
+
+namespace negocio
+{
+    public class CategoriaNegocio
+    {
+        public List<Categoria> listar()
+        {
+            List<Categoria> listaCategoria = new List<Categoria>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setConsulta("SELECT ID, DESCRIPCION FROM CATEGORIAS");
+                datos.ejecutarLectura();
+
+
+                while (datos.Lector.Read())
+                {
+                    Categoria categoria = new Categoria();
+                    categoria.Id = (int)datos.Lector["Id"];
+                    categoria.Descripcion = (string)datos.Lector["Descripcion"];
+
+                    listaCategoria.Add(categoria);
+
+                }
+
+                return listaCategoria;
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+
+            }
+        }
+
+        public void agregar(Categoria nuevo)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setConsulta("INSERT INTO CATEGORIAS (DESCRIPCION) VALUES('" + nuevo.Descripcion + "')");
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void modificar(Categoria categoria)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setConsulta("UPDATE CATEGORIAS SET DESCRIPCION = '" + categoria.Descripcion + "' WHERE ID = '" + categoria.Id + "'");
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+    }
+}
