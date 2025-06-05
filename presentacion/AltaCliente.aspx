e9fe3297fc7754f6866b222eacae407266f8e3ae
nicolas zabala <nicozb.v@gmail.com>
2025-06-05 19:43:50 -0300

    ventana de alta cliente

---
+++ presentacion/AltaCliente.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master.Master" AutoEventWireup="true" CodeBehind="AltaCliente.aspx.cs" Inherits="presentacion.AltaCliente" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h2>Formulario de Alta Cliente</h2>
+    <div class="row">
+        <div class="col-6">
+            <div class="mb-3">
+                <label for="txtNombreCliente" class="form-label">Nombre</label>
+                <asp:textbox runat="server" ID="txtNombreCliente" cssclass="form-control" />
+            </div>
+            <div class="mb-3">
+                <label for="txtCuilCliente" class="form-label">Cuil/Cuit</label>
+                <asp:textbox runat="server" ID="txtCuilCliente" cssclass="form-control" />
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Email</label>
+                <asp:textbox runat="server" ID="txtEmailCliente" cssclass="form-control" placeholder="ejemplo@email.com"/>
+            </div>
+            <div class="mb-3">
+                <asp:button text="Agregar" runat="server" ID="btnAgregarCliente"/>
+            </div>
+        </div>
+    </div>
+</asp:Content>
---
+++ presentacion/AltaCliente.aspx.designer.cs
@@ -0,0 +1,53 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace presentacion
+{
+
+
+    public partial class AltaCliente
+    {
+
+        /// <summary>
+        /// Control txtNombreCliente.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNombreCliente;
+
+        /// <summary>
+        /// Control txtCuilCliente.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCuilCliente;
+
+        /// <summary>
+        /// Control txtEmailCliente.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtEmailCliente;
+
+        /// <summary>
+        /// Control btnAgregarCliente.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAgregarCliente;
+    }
+}
