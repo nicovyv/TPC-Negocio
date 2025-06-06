303f3d1090f56ae41dc72ca02a3c7300c0c2362b
Ivan Baigorria <ivann.baigorria@gmail.com>
2025-06-06 18:55:11 -0300

    Se agrega la pantalla de Perfil

---
+++ presentacion/Perfil.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master.Master" AutoEventWireup="true" CodeBehind="Perfil.aspx.cs" Inherits="presentacion.Perfil" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h2>Perfil</h2>
+
+<div class="row">
+    <div class="col-md-5">
+        <label>Usuario</label>
+        <asp:Label ID="lblUsuario" runat="server" />
+    </div>
+    <div class="col-md-5">
+    </div>
+    <div class="col-md-5">
+        <label>Nombre</label>
+        <asp:TextBox ID="txtBoxNombre" runat="server" />
+        <label>Contraseña</label>
+        <asp:TextBox ID="txtBoxContraseña" runat="server" />
+    </div>
+    <div class="col">
+        <asp:Button Text="Guardar" runat="server" />
+    </div>
+
+
+</div>
+</asp:Content>
---
+++ presentacion/Perfil.aspx.designer.cs
@@ -0,0 +1,44 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace presentacion
+{
+
+
+    public partial class Perfil
+    {
+
+        /// <summary>
+        /// Control lblUsuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblUsuario;
+
+        /// <summary>
+        /// Control txtBoxNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtBoxNombre;
+
+        /// <summary>
+        /// Control txtBoxContraseña.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtBoxContraseña;
+    }
+}
