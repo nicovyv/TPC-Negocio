c4b8c6080da3a585ad1cf7ff49bdf69d49de2c87
benitex-dev <benitezjorgedev@gmail.com>
2025-06-07 11:35:13 -0300

    se agrega clase security

---
+++ negocio/Security.cs
@@ -0,0 +1,72 @@
+﻿using dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace negocio
+{
+    public class Security
+    {
+        public static bool isAdmin(object user)
+        {
+            Usuario usuario;
+
+            if (user != null)
+            {
+                usuario = (Usuario)user;
+            }
+            else
+            {
+                usuario = null;
+            }
+
+            if (usuario.TipoUsuario.Equals(2) )
+            {
+                return true;
+            }
+            else { return false; }
+        }
+        public static bool isGerente(object user)
+        {
+            Usuario usuario;
+
+            if (user != null)
+            {
+                usuario = (Usuario)user;
+            }
+            else
+            {
+                usuario = null;
+            }
+
+            if (usuario.TipoUsuario.Equals(3))
+            {
+                return true;
+            }
+            else { return false; }
+        }
+        public static bool isLogin(object user)
+        {
+            Usuario usuario;
+
+            if (user != null)
+            {
+
+                usuario = (Usuario)user;
+            }
+            else
+            {
+                usuario = null;
+            }
+
+            // Usuario usuario = Session["usuario"] != null ? (Usuario)Session["usuario"] : null;
+            if (usuario != null && usuario.Id != 0)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+    }
+}
