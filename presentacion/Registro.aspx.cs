cfe226ab9a4fd304a74af427c9e860fa68d8ac74
benitex-dev <benitezjorgedev@gmail.com>
2025-06-07 15:09:55 -0300

    se agregan validaciones para la pantalla de Registro

--- presentacion/Registro.aspx.cs
+++ presentacion/Registro.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using negocio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,19 @@ namespace presentacion
         protected void Page_Load(object sender, EventArgs e)
         {
 
+            try
+            {
+                if (Security.isLogin(Session["usuario"]))
+
+                    Response.Redirect("Productos.aspx", false);
+
+            }
+            catch (Exception ex)
+            {
+
+                Session.Add("error", ex);
+                Response.Redirect("Error.aspx");
+            }
         }
     }
 }
