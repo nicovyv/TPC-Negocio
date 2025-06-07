d92c8c0099cc087c3a146046cbf6e1d5d672c7ca
benitex-dev <benitezjorgedev@gmail.com>
2025-06-07 09:20:12 -0300

    Modificación en clase Proveedor - Se agregan propertys Telefono y Email para que hayan datos de contacto del proveedor

--- dominio/Proovedor.cs
+++ dominio/Proovedor.cs
@@ -5,5 +5,7 @@
         public int Id { get; set; }
         public string Nombre { get; set; }
         public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
     }
 }
