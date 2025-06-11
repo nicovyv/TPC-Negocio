94c4177f2d4a2e6d0018b0cd3a2f20bea2c9f908
nicolas zabala <nicozb.v@gmail.com>
2025-06-11 16:11:34 -0300

    se agrega clase ItemVenta

---
+++ dominio/ItemVenta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace dominio
+{
+    public class ItemVenta
+    {
+        public int Id { get; set; }
+        public Producto producto { get; set; }
+        public int cantidad { get; set; }
+        public decimal precioUnidad { get; set; }
+    }
+}
