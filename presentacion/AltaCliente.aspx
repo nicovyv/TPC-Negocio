525a6e5fd291e04db47d0f4d92a80e2ca5bffcea
Ivan Baigorria <ivann.baigorria@gmail.com>
2025-06-20 18:03:30 -0300

    Se agregan validaciones al formulario de AltaCliente.aspx, tambien se muestra el formulario centrado

--- presentacion/AltaCliente.aspx
+++ presentacion/AltaCliente.aspx
@@ -3,29 +3,37 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    <asp:Label runat="server" ID="lblTitulo" CssClass="h2" Text="Formulario de Alta Cliente"></asp:Label>
-    
-    <div class="row">
+
+
+    <div class="d-flex justify-content-center">
+
         <div class="col-6">
             <div class="mb-3">
+                <asp:Label runat="server" ID="lblTitulo" CssClass="h2" Text="Formulario de Alta Cliente"></asp:Label>
+            </div>
+            <div class="mb-3">
+                <asp:Label ID="lblValidarCuit" runat="server" Visible="false" CssClass="alert alert-danger" />
+            </div>
+            <div class="mb-3">
                 <label for="txtNombreCliente" class="form-label">Nombre</label>
-                <asp:TextBox runat="server" ID="txtNombreCliente" CssClass="form-control" />
+                <asp:TextBox runat="server" ID="txtNombreCliente" maxlength="30" placeholder="Nombre" CssClass="form-control" />
             </div>
             <div class="mb-3">
                 <label for="txtCuilCliente" class="form-label">Cuil/Cuit</label>
-                <asp:TextBox runat="server" ID="txtCuilCliente" CssClass="form-control" />
+                <asp:TextBox runat="server" ID="txtCuilCliente" MaxLength="13" placeholder="20-00000000-5" CssClass="form-control" />
             </div>
             <div class="mb-3">
                 <label for="txtDireccion" class="form-label">Direccion</label>
-                <asp:TextBox runat="server" ID="txtDireccion" CssClass="form-control" />
+                <asp:TextBox runat="server" ID="txtDireccion" maxlength="30" placeholder="Calle 1234, Localidad" CssClass="form-control" />
             </div>
             <div class="mb-3">
                 <label for="txtTelefono" class="form-label">Telefono</label>
-                <asp:TextBox runat="server" ID="txtTelefono" CssClass="form-control" />
+                <asp:TextBox runat="server" ID="txtTelefono" pattern="\d{11}" 
+             title="Ingrese solo números, exactamente 11 dígitos" maxlength="11" placeholder="1130000000" CssClass="form-control" />
             </div>
             <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">Email</label>
-                <asp:TextBox runat="server" ID="txtEmailCliente" CssClass="form-control" placeholder="ejemplo@email.com" />
+                <label for="txtTelefono"  class="form-label">Email</label>
+                <asp:TextBox runat="server" ID="txtEmailCliente" title="Por ejemplo: email@dominio.com" maxlength="30" CssClass="form-control" pattern="[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*@[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{1,5}" AutoCompleteType="Email" placeholder="email@dominio.com" />
             </div>
             <div class="mb-3">
                 <asp:Button class="btn btn-dark" Text="Registrar Cliente" runat="server" ID="btnAgregarCliente" OnClick="btnAgregarCliente_Click" />
--- presentacion/AltaCliente.aspx.cs
+++ presentacion/AltaCliente.aspx.cs
@@ -1,11 +1,13 @@
-﻿using System;
+﻿using dominio;
+using negocio;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using dominio;
-using negocio;
 namespace presentacion
 {
     public partial class AltaCliente : System.Web.UI.Page
@@ -25,41 +27,105 @@ namespace presentacion
                     Cliente seleccionado = (negocio.listar(id))[0];
 
                     Session.Add("clienteSeleccionado", seleccionado);
-                    
+
                     txtNombreCliente.Text = seleccionado.Nombre;
                     txtCuilCliente.Text = seleccionado.CuilCuit;
                     txtDireccion.Text = seleccionado.Direccion;
                     txtEmailCliente.Text = seleccionado.Email;
                     txtTelefono.Text = seleccionado.Telefono.ToString();
-                }            
+                }
             }
+        }
+
+        public bool ValidarCamposVacios(object control)
+        {
+            if (control is TextBox texto)
+            {
+                if (string.IsNullOrEmpty(texto.Text))
+                    return true;
+                else
+                    return false;
+
+            }
+            return false;
+        }
+
+        public static bool validarNumero(object control)
+        {
+            if (control is TextBox texto)
+            {
+                if (texto.Text.Length == 11 && texto.Text.All(char.IsDigit))
+                    return true;
+                else
+                    return false;
+            }
+
+            return false;
+        }
+        public bool ValidarCuit(string cuit)
+        {
+            if (!string.IsNullOrWhiteSpace(cuit))
+            {
+                string sinGuion = cuit.Replace("-", "");
+                if (sinGuion.All(char.IsDigit))
+                {
+                    if (sinGuion.Length == 11) return true;
+                }
+            }
+            return false;
         }
 
         protected void btnAgregarCliente_Click(object sender, EventArgs e)
         {
-            Cliente nuevo = new Cliente();
-            ClienteNegocio clienteNegocio = new ClienteNegocio();
-
-            nuevo.Nombre = txtNombreCliente.Text;
-            nuevo.CuilCuit = txtCuilCliente.Text;
-            nuevo.Direccion = txtDireccion.Text;
-            nuevo.Telefono = int.Parse(txtTelefono.Text);
-            nuevo.Email = txtEmailCliente.Text;
-
-            if (Request.QueryString["id"] != null)
+            try
             {
-                nuevo.Id = int.Parse(Request.QueryString["id"]);
-                clienteNegocio.modificarCliente(nuevo);
-                Session.Remove("clienteSeleccionado");
-            }
+                if (ValidarCamposVacios(txtTelefono) || ValidarCamposVacios(txtDireccion) ||
+                    ValidarCamposVacios(txtCuilCliente) || ValidarCamposVacios(txtEmailCliente) || ValidarCamposVacios(txtNombreCliente))
+                {
+                    lblValidarCuit.Visible = true;
+                    lblValidarCuit.Text = "!Atención! Se deben completar todos los datos, por favor";
+                    return;
+                }
 
-            else
+                if (!ValidarCuit(txtCuilCliente.Text))
+                {
+                    lblValidarCuit.Visible = true;
+                    lblValidarCuit.Text = "CUIT/CUIL inválido. Debe contener 11 dígitos";
+                    return;
+                }
+
+
+              
+                Cliente nuevo = new Cliente();
+                ClienteNegocio clienteNegocio = new ClienteNegocio();
+
+                nuevo.Nombre = txtNombreCliente.Text;
+                nuevo.CuilCuit = txtCuilCliente.Text;
+                nuevo.Direccion = txtDireccion.Text;
+                nuevo.Telefono = int.Parse(txtTelefono.Text);
+                nuevo.Email = txtEmailCliente.Text;
+
+                if (Request.QueryString["id"] != null)
+                {
+                    nuevo.Id = int.Parse(Request.QueryString["id"]);
+                    clienteNegocio.modificarCliente(nuevo);
+                    Session.Remove("clienteSeleccionado");
+                }
+
+                else
+                {
+                    clienteNegocio.agregarCliente(nuevo);
+                }
+
+                Response.Redirect("Clientes.aspx");
+                Context.ApplicationInstance.CompleteRequest();
+            }
+            catch (Exception ex)
             {
-                clienteNegocio.agregarCliente(nuevo);
-            }
 
-            Response.Redirect("Clientes.aspx");
-            Context.ApplicationInstance.CompleteRequest();
+                throw ex;
+            }
+           
             
         }
     }
