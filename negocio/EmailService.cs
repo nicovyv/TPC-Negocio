6ba1cccdbede58d07c094f43861ae13e7ef7e306
benitex-dev <benitezjorgedev@gmail.com>
2025-06-21 17:11:50 -0300

    se agrega clase EmailService, con funcionalidades para enviar email, armar correo

---
+++ negocio/EmailService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace negocio
+{
+    public class EmailService
+    {
+        private MailMessage email;
+        private SmtpClient server;
+
+        public EmailService()
+        {
+            server = new SmtpClient();
+            server.Credentials = new NetworkCredential("a1b5de55e9cbf5", "ad5599865c092b");
+            server.EnableSsl = true;
+            server.Port = 2525;
+            server.Host = "smtp.mailtrap.io";
+        }
+        public void armarCorreo(string emailDestino, string asunto, string cuerpo)
+        {
+            email = new MailMessage();
+            email.From = new MailAddress("noresponder@catalogoweb.com");
+            email.To.Add(emailDestino);
+            email.Subject = asunto;
+            email.IsBodyHtml = true;
+            //email.Body = "<h1>Reporte de materias a las que se inscribio...</h1> <br> Hola...";
+            email.Body = cuerpo;
+
+        }
+        public void enviarMail()
+        {
+            try
+            {
+                server.Send(email);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
