6f7b5857423ee22284eba77a6178d4dbcc4cec4f
benitex-dev <benitezjorgedev@gmail.com>
2025-06-23 21:27:47 -0300

    se agrega Apllication_Error Global.asax.cs para manejo de errores generico

--- presentacion/Global.asax.cs
+++ presentacion/Global.asax.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using negocio;
+using System;
+using System.Web;
 
 namespace presentacion
 {
@@ -6,6 +8,13 @@ namespace presentacion
     {
         protected void Application_Start(object sender, EventArgs e)
         {
+        }
+        void Application_Error(object sender, EventArgs e)
+        {
+            Exception exc = Server.GetLastError();
+
+            Session.Add("error", Security.ManejoError(exc));
+            Response.Redirect("Error.aspx");
         }
     }
 }
