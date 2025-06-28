38e37dff05070e11d119814c1709d5fe7af805e8
Ivan Baigorria <ivann.baigorria@gmail.com>
2025-06-28 11:03:41 -0300

    se agrega un dropdown en el nav de compras y en el se visualiza nueva compra y ver compras

--- presentacion/Compras.aspx.cs
+++ presentacion/Compras.aspx.cs
@@ -12,7 +12,7 @@ namespace presentacion
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            cargarCompras();
+           // cargarCompras();
         }
 
         private void cargarCompras()
